0b792c50b4ad10676a09153f2f33ae3eaf86bd0d
icuxika <icuxika@outlook.com>
2023-09-21 14:21:05 +0800

    set UTF-8

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[]) {
+  // 使 qDebug 输出不乱码
+  QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+  QTextCodec::setCodecForLocale(codec);
+
   QApplication a(argc, argv);
   MainWindow w;
   w.show();
